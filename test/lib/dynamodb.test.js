12ad44ed5e3fbb1f0d89cf75315127cffe261003
Richard Hyatt <richard@vandium.io>
2018-11-12 18:26:51 -0500

    finished dynamodb

--- test/lib/dynamodb.test.js
+++ test/lib/dynamodb.test.js
@@ -12,18 +12,36 @@ describe( 'lib/dynamodb', function() {
 
     describe( 'DynamoDBEventMock', function() {
 
+        describe( '.eventName', function() {
+
+            it( 'normal operation', function() {
+
+                let instance = new DynamoDBEventMock();
+
+                // default
+                expect( instance.currentRecord.eventName ).to.equal( 'INSERT' );
+
+                let returnValue = instance.eventName( 'REMOVE' );
+
+                expect( returnValue ).to.equal( instance );
+                expect( instance.currentRecord.eventName ).to.equal( 'REMOVE' );
+            });
+        });
+
         describe( '.build', function() {
 
             it( 'defaults', function() {
 
                 let event = new DynamoDBEventMock().build();
 
-                console.log( event );
+                expect( eventIdentifier.identify( event ) ).to.equal( 'dynamodb' );
             });
 
-            it( 'normal operation', function() {
+            it( 'validate SizeBytes', function() {
 
-                let event = new DynamoDBEventMock()
+                // sample date from:
+                // https://docs.aws.amazon.com/lambda/latest/dg/eventsources.html
+                let event1 = new DynamoDBEventMock()
                                 .keys( {
                                     Id: {
                                         N: '101'
@@ -39,7 +57,144 @@ describe( 'lib/dynamodb', function() {
                                 })
                                 .build();
 
-                expect( event.Records[0].SizeBytes ).to.equal( 26 );
+                expect( event1.Records[0].dynamodb.SizeBytes ).to.equal( 26 );
+                expect( eventIdentifier.identify( event1 ) ).to.equal( 'dynamodb' );
+
+                let event2 = new DynamoDBEventMock()
+                                    .keys( {
+                                      "Id": {
+                                        "N": "101"
+                                      }
+                                    })
+                                    .oldImage( {
+                                      "Message": {
+                                        "S": "New item!"
+                                      },
+                                      "Id": {
+                                        "N": "101"
+                                      }
+                                    })
+                                    .newImage( {
+                                      "Message": {
+                                        "S": "This item has changed"
+                                      },
+                                      "Id": {
+                                        "N": "101"
+                                      }
+                                    })
+                                    .build();
+
+                expect( event2.Records[0].dynamodb.SizeBytes ).to.equal( 59 );
+                expect( eventIdentifier.identify( event2 ) ).to.equal( 'dynamodb' );
+
+                let event3 = new DynamoDBEventMock()
+                                    .keys( {
+                                      Id: {
+                                          N: '101'
+                                      }
+                                    })
+                                    .newImage( {
+                                        Message: {
+                                            SS: ['Item1','Item2']
+                                        },
+                                        Counts: {
+                                            NS: ['1','2']
+                                        },
+                                        Data: {
+                                            BS: ['dGVzdA==']
+                                        },
+                                        Nice: {
+                                            BOOL: true
+                                        },
+                                        Id: {
+                                            N: '101'
+                                        }
+                                    })
+                                    .build();
+
+                expect( event3.Records[0].dynamodb.SizeBytes ).to.equal( 45 );
+                expect( eventIdentifier.identify( event3 ) ).to.equal( 'dynamodb' );
+            });
+
+            it( 'validate StreamViewType', function() {
+
+                // sample date from:
+                // https://docs.aws.amazon.com/lambda/latest/dg/eventsources.html
+                let event1 = new DynamoDBEventMock()
+                                .keys( {
+                                    Id: {
+                                        N: '101'
+                                    }
+                                })
+                                .build();
+
+                expect( event1.Records[0].dynamodb.StreamViewType ).to.equal( 'KEYS_ONLY' );
+                expect( eventIdentifier.identify( event1 ) ).to.equal( 'dynamodb' );
+
+                let event2 = new DynamoDBEventMock()
+                                    .keys( {
+                                      "Id": {
+                                        "N": "101"
+                                      }
+                                    })
+                                    .oldImage( {
+                                      "Message": {
+                                        "S": "New item!"
+                                      },
+                                      "Id": {
+                                        "N": "101"
+                                      }
+                                    })
+                                    .build();
+
+                expect( event2.Records[0].dynamodb.StreamViewType ).to.equal( 'OLD_IMAGE' );
+                expect( eventIdentifier.identify( event2 ) ).to.equal( 'dynamodb' );
+
+                let event3 = new DynamoDBEventMock()
+                                    .keys( {
+                                      Id: {
+                                          N: '101'
+                                      }
+                                    })
+                                    .newImage( {
+                                        Message: {
+                                            S: 'New item!'
+                                        },
+                                        Id: {
+                                            N: '101'
+                                        }
+                                    })
+                                    .build();
+
+                expect( event3.Records[0].dynamodb.StreamViewType ).to.equal( 'NEW_IMAGE' );
+                expect( eventIdentifier.identify( event3 ) ).to.equal( 'dynamodb' );
+
+                let event4 = new DynamoDBEventMock()
+                                    .keys( {
+                                      "Id": {
+                                        "N": "101"
+                                      }
+                                    })
+                                    .oldImage( {
+                                      "Message": {
+                                        "S": "New item!"
+                                      },
+                                      "Id": {
+                                        "N": "101"
+                                      }
+                                    })
+                                    .newImage( {
+                                      "Message": {
+                                        "S": "This item has changed"
+                                      },
+                                      "Id": {
+                                        "N": "101"
+                                      }
+                                    })
+                                    .build();
+
+                expect( event4.Records[0].dynamodb.StreamViewType ).to.equal( 'NEW_AND_OLD_IMAGES' );
+                expect( eventIdentifier.identify( event4 ) ).to.equal( 'dynamodb' );
             });
         });
     });
